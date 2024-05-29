2c7a295b520d299b8b56bde8684ced54f1ce5cea
samuel234423 <135075446+samuel234423@users.noreply.github.com>
2024-05-28 22:46:33 -0300

    Add files via upload

---
+++ Quiz 2/Quiz.eu/src/Samuca.java
@@ -0,0 +1,297 @@
+import java.util.Scanner;
+
+public class Samuca {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String questao1 = "qual o objeto mais importante de um computador?";
+        System.out.println(questao1);
+        System.out.println("[A] cpu");
+        System.out.println("[B] gpu");
+        System.out.println("[C] ram");
+        System.out.println("[D] ssd");
+        System.out.println("[E] teclado");
+        System.out.println("Digite sua Resposta: ");
+        String resposta1 = scanner.nextLine();
+        if (resposta1.equalsIgnoreCase("A")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada! Tente Novamente");
+        }
+
+
+        String questao2 = "Qual é o sistema operacional mais usado em desktops?";
+        System.out.println(questao2);
+        System.out.println("[A] macOS");
+        System.out.println("[B] Windows");
+        System.out.println("[C] Linux");
+        System.out.println("[D] Chrome OS");
+        System.out.println("[E] FreeBSD");
+        System.out.println("Digite sua Resposta: ");
+        String resposta2 = scanner.nextLine();
+        if (resposta2.equalsIgnoreCase("B")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada! Tente Novamente");
+        }
+
+
+        String questao3 = "Qual é a unidade de armazenamento de dados mais rápida?";
+        System.out.println(questao3);
+        System.out.println("[A] HDD");
+        System.out.println("[B] SSD");
+        System.out.println("[C] DVD");
+        System.out.println("[D] CD");
+        System.out.println("[E] Blu-ray");
+        System.out.println("Digite sua Resposta: ");
+        String resposta3 = scanner.nextLine();
+        if (resposta3.equalsIgnoreCase("B")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada! Tente Novamente");
+        }
+
+
+        String questao4 = "O que significa a sigla RAM?";
+        System.out.println(questao4);
+        System.out.println("[A] Random Access Memory");
+        System.out.println("[B] Read Access Memory");
+        System.out.println("[C] Rapid Access Memory");
+        System.out.println("[D] Random Allocate Memory");
+        System.out.println("[E] Read Allocate Memory");
+        System.out.println("Digite sua Resposta: ");
+        String resposta4 = scanner.nextLine();
+        if (resposta4.equalsIgnoreCase("A")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada! Tente Novamente");
+        }
+
+
+        String questao5 = "Qual empresa desenvolveu o processador Ryzen?";
+        System.out.println(questao5);
+        System.out.println("[A] Intel");
+        System.out.println("[B] AMD");
+        System.out.println("[C] NVIDIA");
+        System.out.println("[D] Qualcomm");
+        System.out.println("[E] IBM");
+        System.out.println("Digite sua Resposta: ");
+        String resposta5 = scanner.nextLine();
+        if (resposta5.equalsIgnoreCase("B")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada! Tente Novamente");
+        }
+
+
+        String questao6 = "Qual porta é geralmente usada para conexões de internet com fio?";
+        System.out.println(questao6);
+        System.out.println("[A] USB");
+        System.out.println("[B] HDMI");
+        System.out.println("[C] Ethernet");
+        System.out.println("[D] VGA");
+        System.out.println("[E] DVI");
+        System.out.println("Digite sua Resposta: ");
+        String resposta6 = scanner.nextLine();
+        if (resposta6.equalsIgnoreCase("C")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada! Tente Novamente");
+        }
+
+
+        String questao7 = "Qual é o sistema de arquivos padrão do Windows?";
+        System.out.println(questao7);
+        System.out.println("[A] NTFS");
+        System.out.println("[B] EXT4");
+        System.out.println("[C] HFS+");
+        System.out.println("[D] FAT32");
+        System.out.println("[E] APFS");
+        System.out.println("Digite sua Resposta: ");
+        String resposta7 = scanner.nextLine();
+        if (resposta7.equalsIgnoreCase("A")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada! Tente Novamente");
+        }
+
+
+        String questao8 = "Qual é o principal componente de uma placa-mãe?";
+        System.out.println(questao8);
+        System.out.println("[A] CPU");
+        System.out.println("[B] Chipset");
+        System.out.println("[C] RAM");
+        System.out.println("[D] GPU");
+        System.out.println("[E] Fonte de alimentação");
+        System.out.println("Digite sua Resposta: ");
+        String resposta8 = scanner.nextLine();
+        if (resposta8.equalsIgnoreCase("B")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada! Tente Novamente");
+        }
+
+
+        String questao9 = "Qual é a função de uma GPU?";
+        System.out.println(questao9);
+        System.out.println("[A] Processamento de gráficos");
+        System.out.println("[B] Armazenamento de dados");
+        System.out.println("[C] Controle de áudio");
+        System.out.println("[D] Gerenciamento de energia");
+        System.out.println("[E] Conexão com a internet");
+        System.out.println("Digite sua Resposta: ");
+        String resposta9 = scanner.nextLine();
+        if (resposta9.equalsIgnoreCase("A")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada! Tente Novamente");
+        }
+
+
+        String questao10 = "Qual linguagem de programação é conhecida por sua portabilidade e uso em sistemas embarcados?";
+        System.out.println(questao10);
+        System.out.println("[A] Java");
+        System.out.println("[B] Python");
+        System.out.println("[C] C");
+        System.out.println("[D] Ruby");
+        System.out.println("[E] JavaScript");
+        System.out.println("Digite sua Resposta: ");
+        String resposta10 = scanner.nextLine();
+        if (resposta10.equalsIgnoreCase("C")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada! Tente Novamente");
+        }
+
+
+        String questao11 = "O que é um SSD?";
+        System.out.println(questao11);
+        System.out.println("[A] Solid State Drive");
+        System.out.println("[B] Super Speed Drive");
+        System.out.println("[C] Static Storage Device");
+        System.out.println("[D] Solid Storage Disk");
+        System.out.println("[E] System Storage Device");
+        System.out.println("Digite sua Resposta: ");
+        String resposta11 = scanner.nextLine();
+        if (resposta11.equalsIgnoreCase("A")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada! Tente Novamente");
+        }
+
+
+        String questao12 = "Qual empresa é conhecida por fabricar placas de vídeo GeForce?";
+        System.out.println(questao12);
+        System.out.println("[A] Intel");
+        System.out.println("[B] AMD");
+        System.out.println("[C] NVIDIA");
+        System.out.println("[D] Asus");
+        System.out.println("[E] MSI");
+        System.out.println("Digite sua Resposta: ");
+        String resposta12 = scanner.nextLine();
+        if (resposta12.equalsIgnoreCase("C")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada! Tente Novamente");
+        }
+
+
+        String questao13 = "O que significa a sigla BIOS?";
+        System.out.println(questao13);
+        System.out.println("[A] Basic Input Output System");
+        System.out.println("[B] Binary Input Output System");
+        System.out.println("[C] Basic Internal Operating System");
+        System.out.println("[D] Basic Input Output Service");
+        System.out.println("[E] Binary Internal Operating System");
+        System.out.println("Digite sua Resposta: ");
+        String resposta13 = scanner.nextLine();
+        if (resposta13.equalsIgnoreCase("A")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada! Tente Novamente");
+        }
+
+
+        String questao14 = "Qual é a função de um firewall?";
+        System.out.println(questao14);
+        System.out.println("[A] Gerenciar senhas");
+        System.out.println("[B] Monitorar o desempenho do sistema");
+        System.out.println("[C] Proteger contra acessos não autorizados");
+        System.out.println("[D] Realizar backups automáticos");
+        System.out.println("[E] Aumentar a velocidade da internet");
+        System.out       .println("Digite sua Resposta: ");
+        String resposta14 = scanner.nextLine();
+        if (resposta14.equalsIgnoreCase("C")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada! Tente Novamente");
+        }
+
+
+        String questao15 = "O que é overclocking?";
+        System.out.println(questao15);
+        System.out.println("[A] Aumentar a temperatura do computador");
+        System.out.println("[B] Reduzir a velocidade do processador");
+        System.out.println("[C] Aumentar a velocidade do processador além das especificações de fábrica");
+        System.out.println("[D] Diminuir o desempenho da placa de vídeo");
+        System.out.println("[E] Desativar o sistema de resfriamento");
+        System.out.println("Digite sua Resposta: ");
+        String resposta15 = scanner.nextLine();
+        if (resposta15.equalsIgnoreCase("C")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada! Tente Novamente");
+        }
+
+
+        String[][] perguntasRespostas = {
+                {"Qual o objeto mais importante de um computador?", "A"},
+                {"Qual é o sistema operacional mais usado em desktops?", "B"},
+                {"Qual é a unidade de armazenamento de dados mais rápida?", "B"},
+                {"O que significa a sigla RAM?", "A"},
+                {"Qual empresa desenvolveu o processador Ryzen?", "B"},
+                {"Qual porta é geralmente usada para conexões de internet com fio?", "C"},
+                {"Qual é o sistema de arquivos padrão do Windows?", "A"},
+                {"Qual é o principal componente de uma placa-mãe?", "B"},
+                {"Qual é a função de uma GPU?", "A"},
+                {"Qual linguagem de programação é conhecida por sua portabilidade e uso em sistemas embarcados?", "C"},
+                {"O que é um SSD?", "A"},
+                {"Qual empresa é conhecida por fabricar placas de vídeo GeForce?", "C"},
+                {"O que significa a sigla BIOS?", "A"},
+                {"Qual é a função de um firewall?", "C"},
+                {"O que é overclocking?", "C"}
+        };
+
+        int corretas = 0;
+        int erradas = 0;
+
+        for (String[] perguntaResposta : perguntasRespostas) {
+            String pergunta = perguntaResposta[0];
+            String opcaoCorreta = perguntaResposta[1];
+
+            System.out.println(pergunta);
+            System.out.println("[A] Opção A");
+            System.out.println("[B] Opção B");
+            System.out.println("[C] Opção C");
+            System.out.println("[D] Opção D");
+            System.out.println("[E] Opção E");
+            System.out.println("Digite sua Resposta: ");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equalsIgnoreCase(opcaoCorreta)) {
+                System.out.println("Resposta Correta!");
+                corretas++;
+            } else {
+                System.out.println("Resposta Errada! Tente Novamente");
+                erradas++;
+            }
+        }
+
+        System.out.println("\nResultados:");
+        System.out.println("Acertos: " + corretas);
+        System.out.println("Erros: " + erradas);
+
+        scanner.close();
+    }
+}
